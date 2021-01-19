2849e1a3ac0d6e2ffb49c22abcdddb699e4c9b43
Noveria <little.noveria@gmail.com>
2021-01-20 01:44:50 +0300

    HW2, task1

---
+++ HW2/HW2.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HW2/HW2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Запросить у пользователя минимальную и максимальную 
+            //температуру за сутки и вывести среднесуточную температуру.
+
+            Console.WriteLine("Введите минимальную температуру за сутки");
+            int minTemperature = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру за сутки");
+            int maxTemperature = Convert.ToInt32(Console.ReadLine());
+            int averageDailyTemperature = (minTemperature + maxTemperature) / 2;
+            Console.WriteLine($"Среднесуточная температура: {averageDailyTemperature}");
+
+            readKey();
+        }
+
+        public static void readKey() {
+            Console.ReadKey();
+        }
+    }
+}
