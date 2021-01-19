04a64c42ec12c7f810da41ff2d6dd50d2153e877
Noveria <little.noveria@gmail.com>
2021-01-20 02:11:18 +0300

    HW2, task 3

--- HW2/HW2.3/Program.cs
+++ HW2/HW2.3/Program.cs
@@ -6,7 +6,34 @@ namespace HW2._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Определить, является ли введённое пользователем число чётным.
+
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            /*
+            switch (number % 2 == 0)
+            {
+                case true:
+                    Console.WriteLine("Число четное");
+                    break;
+                default:
+                    Console.WriteLine("Число не четное");
+                    break;
+            }
+            */
+
+            if (number % 2 == 0)
+                Console.WriteLine("Число четное");
+            else
+                Console.WriteLine("Число не четное");
+
+            readKey();
+        }
+
+        public static void readKey()
+        {
+            Console.ReadKey();
         }
     }
 }
