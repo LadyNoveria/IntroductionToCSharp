1ee023372d838230c1ffcc5971c7943802deaca8
Noveria <little.noveria@gmail.com>
2021-01-24 19:52:18 +0300

    HW3, task 3

--- HW3/HW3.3/Program.cs
+++ HW3/HW3.3/Program.cs
@@ -6,7 +6,28 @@ namespace HW3._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Написать программу, выводящую введенную пользователем строку в обратном
+             *порядке (olleH вместо Hello).*/
+            Console.WriteLine("Введите любую строку");
+            string enteredString = Console.ReadLine();
+
+            //Вывод строки задом наперед
+            stringOutput(enteredString);
+            readKey();
+        }
+
+        //Вывод строки задом наперед
+        private static void stringOutput(string enteredString)
+        {
+            for (int i = enteredString.Length - 1; i >= 0; i--)
+            {
+                Console.Write(enteredString[i]);
+            }
+        }
+
+        public static void readKey()
+        {
+            Console.ReadKey();
         }
     }
 }
