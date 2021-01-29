11cf9ba967f2a59981096a436ec6d0947fad8830
Noveria <little.noveria@gmail.com>
2021-01-29 18:40:01 +0300

    HW4, task 2

--- HW4.1/HW4.2/Program.cs
+++ HW4.1/HW4.2/Program.cs
@@ -6,7 +6,32 @@ namespace HW4._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом, и 
+             * возвращающую число — сумму всех чисел в строке. Ввести данные с клавиатуры и вывести результат 
+             * на экран.*/
+
+            GetNumericalSequence(out string[] arrayOfNumbers);
+            int summ = GetSummOfNumericalSequence(arrayOfNumbers);
+            Console.WriteLine($"Сумма введенных чисел: {summ}");
+            ReadKey();
+        }
+
+        public static void ReadKey() {
+            Console.ReadKey();
+        }
+        public static int GetSummOfNumericalSequence(string[] arrayOfNumbers) {
+            int summ = 0;
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                summ += Convert.ToInt32(arrayOfNumbers[i]);
+            }
+            return summ;
+        }
+        public static string[] GetNumericalSequence(out string[] arrayOfNumbers) {
+            Console.WriteLine("Введите последовательность чисел, разделенных пробелом");
+            string numericalSequence = Console.ReadLine();
+            arrayOfNumbers = numericalSequence.Split(" ");
+            return arrayOfNumbers;
         }
     }
 }
