2fdbb96d5577339cb68f158102f0ebb595da5907
Noveria <little.noveria@gmail.com>
2021-01-29 18:50:56 +0300

    HW4, task4

--- HW4.1/HW4.4/Program.cs
+++ HW4.1/HW4.4/Program.cs
@@ -6,7 +6,21 @@ namespace HW4._4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Написать программу, вычисляющую число Фибоначчи для заданного значения рекурсивным способом. */
+            Console.WriteLine("Введите порядковый номер числа Фибоначчи");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int numFib = fib(number);
+            Console.WriteLine($"Число в порядковом номере {number} = {numFib}");
+            Console.ReadKey();
+        }
+
+        public static int fib(int number)
+        {
+            if (number < 2)
+            {
+                return number;
+            }
+            return fib(number - 1) + fib(number - 2);
         }
     }
 }
