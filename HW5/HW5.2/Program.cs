63f76addc100b1a280208df0ed7f24e44e769473
Noveria <little.noveria@gmail.com>
2021-02-01 09:33:45 +0300

    HW5, task 2

--- HW5/HW5.2/Program.cs
+++ HW5/HW5.2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HW5._2
 {
@@ -6,7 +7,16 @@ namespace HW5._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Написать программу, которая при старте дописывает текущее время в файл «startup.txt».*/
+            string filename = "startup.txt";
+            DateTime date = DateTime.Now;
+            string dateTime = date.ToString();
+            File.AppendAllText(filename, dateTime);
+            File.AppendAllText(filename, Environment.NewLine);
+            //для проверки
+            string fileText = File.ReadAllText(filename);
+            Console.WriteLine($"Текст из файла: {fileText}");
+            Console.ReadKey();
         }
     }
 }
